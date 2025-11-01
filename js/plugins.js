b90a3c2d687b75e3552a493512cf35fcf129416c
YOUSSEF14230 <youssef2952011@gmail.com>
2025-11-01 10:14:07 +0200

    animation website

---
+++ js/plugins.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+ $("button").click(function(){
+  $("div").animate({
+top:'10px',
+left:'600px'
+ } , 2000);
+
+ $("span").animate({
+top:'140px',
+ } , 2000);
+
+ $("p").animate({
+top:'160px',
+ } , 2000);
+
+ $(".two").animate({
+top:'300',
+ } , 2000);
+
+ $("h3").animate({
+top:'250px',
+ } , 2000);
+
+  $(".one").animate({
+top:'500px',
+ } , 2000);
+
+  $("h2").animate({
+top:'450px',
+ } , 2000);
+
+$("button").fadeOut(1000)});
+});
+
